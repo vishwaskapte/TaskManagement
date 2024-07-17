e4c8472a8c9fe4d0ddd08b09d396fe682c572888
Vishwas V. Kapte <vishwas.kapte@outlook.com>
2024-07-17 23:06:43 +0530

    Added DTOs into Project

--- TaskManagement.API/Controllers/TasksController.cs
+++ TaskManagement.API/Controllers/TasksController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TaskManagement.API.Data;
+using TaskManagement.API.Models.DTO;
 
 namespace TaskManagement.API.Controllers
 {
@@ -7,5 +11,55 @@ namespace TaskManagement.API.Controllers
     [ApiController]
     public class TasksController : ControllerBase
     {
+        private readonly TaskManagementDbContext dbContext;
+
+        public TasksController(TaskManagementDbContext DbContext)
+        {
+            dbContext = DbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data From Database - Domain Model
+            var tasks = dbContext.Taskss.ToList();
+
+            //Map Domain Models to DTOs
+            var tasksDto = new List<TasksDto>();
+            foreach (var task in tasks) {
+                tasksDto.Add(new TasksDto()
+                {
+                    TaskId = task.TaskId,
+                    Title = task.Title,
+                    Description = task.Description,
+                    DueDate = task.DueDate,
+                    StatusId = task.StatusId
+                });
+            }
+            return Ok(tasksDto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute]int id)
+        {
+            //Get Data From Database - Domain Model
+            var tasks = dbContext.Taskss.FirstOrDefault(x => x.TaskId == id);
+
+            if (tasks == null) { return NotFound(); }
+
+            //Map Domain Models to DTOs
+            var tasksDto = new TasksDto()
+            {
+                TaskId = tasks.TaskId,
+                Title = tasks.Title,
+                Description = tasks.Description,
+                DueDate = tasks.DueDate,
+                StatusId = tasks.StatusId
+            };
+
+            return Ok(tasksDto);
+
+        }
     }
 }
---
+++ TaskManagement.API/Models/DTO/TasksDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement.API.Models.DTO
+{
+    public class TasksDto
+    {
+        public int TaskId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public int StatusId { get; set; }
+    }
+}
