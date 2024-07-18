f0accce5288ea97f8388106dfe7e8ec762b3cb20
Vishwas V. Kapte <vishwas.kapte@outlook.com>
2024-07-18 12:31:21 +0530

    Added CRUD Operations

--- TaskManagement.API/Controllers/TasksController.cs
+++ TaskManagement.API/Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using TaskManagement.API.Data;
+using TaskManagement.API.Models.Domain;
 using TaskManagement.API.Models.DTO;
 
 namespace TaskManagement.API.Controllers
@@ -60,6 +61,98 @@ namespace TaskManagement.API.Controllers
 
             return Ok(tasksDto);
 
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]AddTaskRequestDto taskRequestDto) 
+        {
+            //Map or convert DTO to Domain Model
+            var taskDomainModel = new Tasks
+            {
+                Title = taskRequestDto.Title,
+                Description = taskRequestDto.Description,
+                DueDate = taskRequestDto.DueDate,
+                StatusId = taskRequestDto.StatusId
+            };
+
+            //Use Domain Model to Create Tasks
+            dbContext.Taskss.Add(taskDomainModel);
+            dbContext.SaveChanges();
+
+            //Map Domain Model to DTO
+
+            var TasksDto = new TasksDto()
+            {
+                TaskId = taskDomainModel.TaskId,
+                Title = taskRequestDto.Title,
+                Description = taskRequestDto.Description,
+                DueDate = taskRequestDto.DueDate,
+                StatusId = taskRequestDto.StatusId
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = taskDomainModel.TaskId }, taskDomainModel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute]int id, [FromBody]UpdateTaskRequestDto updateTaskRequestDto) 
+        {
+            //check if task exist
+            var taskDomainModel = dbContext.Taskss.FirstOrDefault(x => x.TaskId == id);
+
+            if(taskDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //map DTO to Domain Model
+            taskDomainModel.Title = updateTaskRequestDto.Title;
+            taskDomainModel.Description = updateTaskRequestDto.Description;
+            taskDomainModel.DueDate = updateTaskRequestDto.DueDate;
+            taskDomainModel.StatusId = updateTaskRequestDto.StatusId;
+
+            dbContext.SaveChanges();
+
+            //Map Domain Models to DTOs
+            var tasksDto = new TasksDto()
+            {
+                TaskId = taskDomainModel.TaskId,
+                Title = taskDomainModel.Title,
+                Description = taskDomainModel.Description,
+                DueDate = taskDomainModel.DueDate,
+                StatusId = taskDomainModel.StatusId
+            };
+
+            return Ok(tasksDto);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute]int id) 
+        {
+            //check if task exist
+            var taskDomainModel = dbContext.Taskss.FirstOrDefault(x => x.TaskId == id);
+
+            if (taskDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //Delete Tasks
+            dbContext.Taskss.Remove(taskDomainModel);
+            dbContext.SaveChanges();
+
+            //Map Domain Models to DTOs
+            var tasksDto = new TasksDto()
+            {
+                TaskId = taskDomainModel.TaskId,
+                Title = taskDomainModel.Title,
+                Description = taskDomainModel.Description,
+                DueDate = taskDomainModel.DueDate,
+                StatusId = taskDomainModel.StatusId
+            };
+
+            return Ok(tasksDto);
         }
     }
 }
---
+++ TaskManagement.API/Models/DTO/AddTaskRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.API.Models.DTO
+{
+    public class AddTaskRequestDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public int StatusId { get; set; }
+    }
+}
